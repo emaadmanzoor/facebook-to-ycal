c48533682062818abf22ed8fdec8496f9e071d22
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2011-10-11 20:50:54 +0530

    asd

--- app.rb
+++ app.rb
@@ -42,11 +42,13 @@ helpers do
 
   def print_birthdays
     birthday_list = ""
-    @user.friends(:birthday, :name, :id).each do |friend|
-      birthday_list = birthday_list + friend.name.to_s + " --> " + friend.birthday.to_s + " --> " + friend.id.to_s + "\n" 
+    @user.friends(:birthday_date, :name, :id).each do |friend|
+      birthday_list = birthday_list + friend.name.to_s + " --> " + friend.birthday_date.to_s + " --> " + friend.id.to_s + "\n" 
     end
     return birthday_list
   end
+
+  def test
 end
 
 # the facebook session expired! reset ours and restart the process
